5a6f8f524cef309df9980bdeffa057f24e631998
Xiao Shengguang <shengguangxiao@gmail.com>
2022-05-14 12:11:26 +0800

    Add script to generate NFT meta data

--- scripts/GenNFTMetaData.py
+++ scripts/GenNFTMetaData.py
@@ -7,14 +7,14 @@ if not os.path.exists("./artifacts"):
 if not os.path.exists("./artifacts/metadata"):
     os.mkdir("./artifacts/metadata")
 
-for i in range(100):
+for i in range(200):
     meta = dict()
     tokenId = str(i + 1)
-    imageId = str(i + 9900)
-    meta["name"] = "FireNFT#" + imageId
-    meta["description"] = "Limited edition Fire NFT Avatars, to serve as your Gamerse profile picture. This Revolutionising NFT collection has a built-in burn function of 5% $LFG each time its sold on the secondary market. This unique feature creates scarcity and adds deflationary pressure increasing the value of the NFT overtime. Let the Gamerse begin!"
-    meta["image"] = "https://gamerse.mypinata.cloud/ipfs/QmVRGsgbosiwu2StACHDfhHH2obfLEpz5uADRjpcWQK27W/black_" + \
-        imageId + ".png"
+    meta["name"] = "Gamerse Fire NFT V2 #" + tokenId
+    meta["description"] = "Limited edition Fire NFT Avatars, to serve as your Gamerse profile picture. This Revolutionising NFT collection has a built-in burn function of 5% $LFG each time its sold on the secondary market. \nThis unique feature creates scarcity and adds deflationary pressure increasing the value of the NFT overtime. Let the Gamerse begin!"
+    meta["image"] = "https://gamerse.mypinata.cloud/ipfs/QmY76H7xCbYUg331BPT3qhnGH14Pb4Ur9nAUtaJBAM6hxQ/black_" + \
+        tokenId + ".png"
     f = open("./artifacts/metadata/" + tokenId, "w")
     f.write(json.dumps(meta))
     f.close()
+    
